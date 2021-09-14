791d7079fcf8d66a41d1839791e7b7d8eee9848a
dale <stewartdale45@yahoo.com>
2021-09-14 19:26:54 -0400

    Implemented client setup, routing and nav menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// import those pages in App.js
+// then based on the path show each components using react-router compomnents.
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './booking/Home';
+import Login from './auth/Login';
+import Register from './auth/Register';
+import { Link } from 'react-router-dom';
+
+/**
+ * Let's create TopNavigation/mmenu bar so that we can easily TopNavigate between pages
+ * Let's write it in App.js before we move it to its own compoenent.
+ */
+
+const TopNav = () => (
+  <div className="nav bg-light d-flex justify-content-between">
+    <Link className="nav-link" to="/">
+      Home
+    </Link>
+    <Link className="nav-link" to="/login">
+      Login
+    </Link>
+    <Link className="nav-link" to="/register">
+      Register
+    </Link>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {TopNav()}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
