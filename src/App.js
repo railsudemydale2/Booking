6ca5fa43607ec166990868cc74c99c4e3ce19970
dale <stewartdale45@yahoo.com>
2021-09-14 19:47:46 -0400

    Finished completing Nav menu

--- src/App.js
+++ src/App.js
@@ -4,31 +4,17 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './booking/Home';
 import Login from './auth/Login';
 import Register from './auth/Register';
-import { Link } from 'react-router-dom';
+import TopNav from './components/TopNav';
 
 /**
  * Let's create TopNavigation/mmenu bar so that we can easily TopNavigate between pages
  * Let's write it in App.js before we move it to its own compoenent.
  */
 
-const TopNav = () => (
-  <div className="nav bg-light d-flex justify-content-between">
-    <Link className="nav-link" to="/">
-      Home
-    </Link>
-    <Link className="nav-link" to="/login">
-      Login
-    </Link>
-    <Link className="nav-link" to="/register">
-      Register
-    </Link>
-  </div>
-);
-
 function App() {
   return (
     <BrowserRouter>
-      {TopNav()}
+      <TopNav />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
