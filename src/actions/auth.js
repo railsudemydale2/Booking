3a8b74e0f90fc5a2995efca43ebc76b5f891c99e
dale <stewartdale45@yahoo.com>
2021-09-22 23:53:18 -0400

    show User's hotel bookings

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -15,3 +15,5 @@ export const updateUserInLocalStorage = (user, next) => {
     next();
   }
 };
+
+
---
+++ src/stripe/StripeSuccess.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { stripeSuccessRequest } from '../actions/stripe';
+import { LoadingOutlined } from '@ant-design/icons';
+
+const StripeSuccess = ({ match, history }) => {
+  const {
+    auth: { token },
+  } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    // console.log(
+    //   'send this hotelid to backend to create order!!!=====> ',
+    //   match.params.hotelId
+    // );
+    stripeSuccessRequest(token, match.params.hotelId).then((res) => {
+      if (res.data.success) {
+        // console.log('stripe success response', res.data);
+        history.push('/dashboard');
+      } else {
+        history.push('/stripe/cancel');
+      }
+    });
+  }, [match.params.hotelId]);
+  return (
+    <div className="container">
+      <div className="d-flex justify-content-center p-5">
+        <LoadingOutlined className="display-1 text-danger p-5" />
+      </div>
+    </div>
+  );
+};
+
+export default StripeSuccess;
